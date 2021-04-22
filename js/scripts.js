6cf184d4a88495c177d911738e51a6243a425238
e180039 <a.fernandez@student.helmo.be>
2021-04-22 12:42:33 +0200

    mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -91,4 +91,14 @@ $(function() {
           }, 2000);
         }
       }
+
+      const hamburger = document.querySelector(".hamburger");
+      const navMenu = document.querySelector(".navLink");
+
+      hamburger.addEventListener("click", mobileMenu);
+
+      function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+      }
 });
